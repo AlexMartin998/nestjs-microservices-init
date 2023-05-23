9bdd0f93616e7862cd92434bf017381ee655388e
Adrian <alextravc.342@gmail.com>
2023-05-22 19:39:04 -0500

    [ADD]: create order (controller + service)

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,22 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { OrdersModule } from './orders.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
-  await app.listen(3000);
+  const logger = new Logger('Bootstrap');
+
+  // get envV
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
+  await app.listen(PORT);
+  logger.log(`App running on port ${PORT}`);
 }
 bootstrap();
